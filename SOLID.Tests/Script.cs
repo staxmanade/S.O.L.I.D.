bdf1a42c8c1a1582cf1267c5bebfbdfccc424a02
Jason Jarrett <jason@elegantcode.com>
2012-02-16 21:30:37 -0800

    extended the script talk

--- SOLID.Tests/Script.cs
+++ SOLID.Tests/Script.cs
@@ -1,4 +1,65 @@
-﻿/*
+﻿
+
+
+
+
+
+
+/*
+ * Presenter: Jason Jarrett @staxmanade
+ * 
+ * Company: Vertigo
+ * 
+ * 
+ * 
+ */
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
  * Some BAD code...
  * http://vimeo.com/9981123
  * 
@@ -8,6 +69,9 @@
 // This real code - so we have real 'usings' :P
 
 using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Net;
 using NUnit.Framework;
 
 namespace SOLID
@@ -50,28 +114,13 @@ namespace SOLID
 		 * Which tool would you choose to solve a problem?
 		 *  a. The tool that can do it all (but none of them really well)? 
 		 *      It's awkard to hold, not very strong and difficult to use?
+		 *      D:\Code\SOLID\SOLID.Tests\images\_giant_swiss_army_knife_2.jpg
+		 *
 		 *  b. Or the toolbox where each tool is designed to do one specific 
 		 *      thing, and do it well?
 		 * 
 		 * 
-		 * 
-		 * 
-		 * 
-		 * 
-		 * 
-		 * 
-		 * 
-		 * 
-		 * 
-		 * 
-		 * 
-		 * 
-		 * 
-		 * 
-		 * 
-		 * 
 		 */
-
 
 		// SRP sometimes relates to an object's cohesion
 		namespace Cohesion
@@ -191,7 +240,7 @@ namespace SOLID
 				[Test]
 				public void SendAMessageToAll()
 				{
-					MessagePublisher  messagePublisher = new MessagePublisher();
+					MessagePublisher messagePublisher = new MessagePublisher();
 					messagePublisher.PublishToFacebook(_message);
 					messagePublisher.PublishToGooglePlus(_message);
 				}
@@ -286,7 +335,7 @@ namespace SOLID
 	{
 
 		/* 
-		 * OCP
+		 * O.C.P.
 		 * Open/Closed Principal
 		 * 
 		 * 
@@ -332,7 +381,7 @@ namespace SOLID
 
 					formattedMessage.ShouldEqual("Message for Facebook - Hello!").Log();
 				}
-				
+
 				[Test]
 				public void Should_print_a_valid_GooglePlus_formatted_message()
 				{
@@ -363,7 +412,7 @@ namespace SOLID
 
 			public class FacebookPublisher : MessagePublisherBase
 			{ }
-			
+
 			public class GooglePlusPublisher : MessagePublisherBase
 			{
 				protected override string GetPublisherName
@@ -431,8 +480,435 @@ namespace SOLID
 
 
 
+	namespace LiskovSubstitutionPrinciple
+	{
+
+		/* 
+		 * L.S.P.
+		 * Liskov Substitution Principal
+		 * 
+		 * 
+		 * 
+		 * 
+		 *			“What is wanted here is something like the following substitution 
+		 *			property: If for each object o1 of type S there is an object o2 
+		 *			of type T such that for all programs P defined in terms of T, the 
+		 *			behavior of P is unchanged when o1 is substituted for o2 then S is 
+		 *			a subtype of T.”
+		 * 
+		 * 
+		 * Or in a more understandable version from Uncle Bob (Robert C. Martin)
+		 * 
+		 *			“Functions that use pointers or references to base classes must 
+		 *			be able to use objects of derived classes without knowing it.”
+		 * 
+		 */
 
 
+
+
+		namespace LspViolation
+		{
+			public abstract class TheBasePublisher
+			{
+				public abstract void PublishMessage(Message message);
+				public abstract void PublishPhoto(byte[] photo);
+			}
+
+
+			public class FacebookPublisher : TheBasePublisher
+			{
+				public override void PublishMessage(Message message)
+				{
+					message.Text.Log();
+				}
+
+				public override void PublishPhoto(byte[] photo)
+				{
+					"Published photo".Log();
+				}
+			}
+
+			public class TwitterPublisher : TheBasePublisher
+			{
+				public override void PublishMessage(Message message)
+				{
+					message.Text.Log();
+				}
+
+				public override void PublishPhoto(byte[] photo)
+				{
+					throw new NotSupportedException("Twitter cannot accept photos");
+				}
+			}
+
+			public class PublisherTests
+			{
+				private void PublishToAll(IEnumerable<TheBasePublisher> publishers, string input, byte[] photo)
+				{
+					foreach (var publisher in publishers)
+					{
+						publisher.PublishPhoto(photo);
+						publisher.PublishMessage(new Message(input));
+					}
+				}
+
+
+				[Test]
+				public void Should_publish_long_message_to_Facebook()
+				{
+					TheBasePublisher[] publishers = new TheBasePublisher[]
+					                                	{
+					                                		new FacebookPublisher(),
+					                                		new TwitterPublisher(),
+					                                	};
+					PublishToAll(publishers, Lipsum.LongMessage, new byte[] { 1, 2, 3 });
+				}
+			}
+		}
+
+
+
+
+		namespace OnePossibleFixForDesign
+		{
+
+
+
+
+			public abstract class TheBasePublisher
+			{
+				public abstract void PublishMessage(Message message);
+			}
+
+			public abstract class PhotoPublisher : TheBasePublisher
+			{
+				public abstract void PublishPhoto(byte[] photo);
+			}
+
+
+
+
+
+			public class FacebookPublisher : PhotoPublisher
+			{
+				public override void PublishMessage(Message message)
+				{
+					message.Text.Log();
+				}
+
+				public override void PublishPhoto(byte[] photo)
+				{
+					"Published photo".Log();
+				}
+			}
+
+			public class TwitterPublisher : TheBasePublisher
+			{
+				public override void PublishMessage(Message message)
+				{
+					message.Text.Log();
+				}
+
+				// No need to implement un-supported features
+			}
+			
+
+
+
+
+
+
+
+
+			public class PublisherTests
+			{
+				private void PublishToAll(IEnumerable<PhotoPublisher> publishers, string input, byte[] photo)
+				{
+					foreach (var publisher in publishers)
+					{
+						publisher.PublishPhoto(photo);
+						publisher.PublishMessage(new Message(input));
+					}
+				}
+
+
+				[Test]
+				public void Should_publish_long_message_to_Facebook()
+				{
+					var publishers = new PhotoPublisher[]
+					                                	{
+					                                		new FacebookPublisher(),
+					                                		//new TwitterPublisher(), //Cannot publish photo to this service:P
+					                                	};
+					PublishToAll(publishers, Lipsum.LongMessage, new byte[] { 1, 2, 3 });
+				}
+			}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+			public class ExampleOfLSPInDotNet
+			{
+				[Test]
+				public void ThisIsABummer()
+				{
+					// ReadOnlyCollection implements the following interfaces
+					//  IList<T>, ICollection<T>, IEnumerable<T>, IList, ICollection, IEnumerable
+					var readOnlyCollection = new ReadOnlyCollection<int>(new[] { 0, 1, 2 });
+
+					AddItemToList(readOnlyCollection, 1);
+				}
+
+				public void AddItemToList(IList<int> list, int item)
+				{
+					list.Add(item);
+				}
+			}
+		}
+	}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+	namespace DependencyInversionPrinciple
+	{
+
+		/*
+		 * D.I.P.
+		 * Dependency Inversion Principle
+		 * 
+		 * 
+		 * 
+		 *		A. High-level modules should not depend on low-level modules. Both should depend on abstractions.
+		 *		B. Abstractions should not depend upon details. Details should depend upon abstractions.
+		 * 
+		 * 
+		 * 
+		 * 
+		 * 
+		 * 
+		 * 
+		 * 
+		 * 
+		 */
+
+		namespace CoupledCodeWithNoAbstraction
+		{
+			public class Settings
+			{
+				public string FacebookPostUrl
+				{
+					get { return "http://facebook.com/SomeUrlIMadeUp"; }
+				}
+			}
+
+			public class FacebookPublisher
+			{
+				public void Publish(string message)
+				{
+					var settings = new Settings();
+
+					var postUrl = settings.FacebookPostUrl;
+
+					("Posted message to [" + message + "]" + postUrl).Log();
+				}
+			}
+
+
+			public class FacebookPublisherTests
+			{
+				[Test]
+				public void PublishTest()
+				{
+					var facebookPublisher = new FacebookPublisher();
+					facebookPublisher.Publish("Hola!");
+				}
+			}
+		}
+
+
+		namespace DecoupledCodeWithSomeAbstraction
+		{
+			public interface ISettings
+			{
+				string FacebookPostUrl { get; }
+			}
+
+			public class Settings : ISettings
+			{
+				public string FacebookPostUrl
+				{
+					get { return "http://facebook.com/SomeUrlIMadeUp"; }
+				}
+			}
+			
+			public class TestSettings : ISettings
+			{
+				public string FacebookPostUrl
+				{
+					get { return "http://localhost/MyTestLocation"; }
+				}
+			}
+
+			public class FacebookPublisher
+			{
+				private readonly ISettings _settings;
+
+				public FacebookPublisher(ISettings settings)
+				{
+					_settings = settings;
+				}
+
+				public void Publish(string message)
+				{
+					var postUrl = _settings.FacebookPostUrl;
+
+					("Posted message to [" + message + "]" + postUrl).Log();
+				}
+			}
+
+
+			public class FacebookPublisherTests
+			{
+				[Test]
+				public void PublishTest()
+				{
+					//var settings = new Settings();
+					var settings = new TestSettings();
+
+					var facebookPublisher = new FacebookPublisher(settings);
+
+					facebookPublisher.Publish("Hola!");
+				}
+			}
+		}
+
+	}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+	namespace InterfaceSegregationPrinciple
+	{
+		/*
+		 * I.S.P.
+		 * Interface Segregation Principle
+		 * 
+		 * 
+		 * 
+		 *		"Clients should not be forced to depend upon interfaces that they don't use."
+		 * 
+		 * 
+		 * 
+		 * 
+		 * 
+		 * 
+		 * 
+		 * 
+		 * 
+		 */
+
+
+	}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+	public class Lipsum
+	{
+		public static string LongMessage
+		{
+			get
+			{
+				return @"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis nec blandit augue. Pellentesque metus purus, pellentesque at imperdiet sit amet, tempus at massa. Pellentesque sagittis faucibus ornare. Pellentesque interdum sem vel nisl interdum vestibulum pulvinar at neque. Sed pellentesque, elit id tempus accumsan, arcu augue dapibus nisi, vel sollicitudin ligula eros eu sem. Ut nec urna vel massa commodo rhoncus ac in est. Sed pulvinar justo vitae nisi malesuada porta. Aliquam sed dignissim metus. Cras a pulvinar nulla. Aliquam convallis mollis scelerisque. Nulla facilisi. Aenean sed tortor in velit facilisis blandit sed nec augue.
+
+In diam nisi, tincidunt at bibendum eget, tempor sit amet nisi. Duis sollicitudin, lacus ac lacinia feugiat, nunc leo bibendum mauris, at pulvinar lorem ipsum in justo. Vivamus faucibus congue velit, in congue ligula congue in. Praesent vel metus quis dui viverra mattis. Donec aliquam est nec nisi rhoncus tristique. Phasellus porttitor leo in leo volutpat fermentum. Nullam viverra egestas lectus eu tempus. Etiam ultrices risus vitae nulla tincidunt convallis. Sed egestas tempus imperdiet. Cras consequat varius tempor. Nam sit amet velit eget mauris rhoncus iaculis ut eu tellus. Donec vehicula blandit auctor. Donec et congue quam. Curabitur luctus fringilla quam ut semper.";
+			}
+		}
+	}
 
 
 
